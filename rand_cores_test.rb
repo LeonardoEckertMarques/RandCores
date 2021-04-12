dd811b2e13418707ca7bfba888501f7175301c26
LeonardoEckertMarques <54690623+LeonardoEckertMarques@users.noreply.github.com>
2021-04-12 16:41:26 -0300

    rand_cores_test.rb

---
+++ rand_cores_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+
+require_relative 'rand_cores'
+
+class RandCoresTest < Minitest::Test
+
+	def setup
+		@rand_cores = RandCores.new
+	end
+	
+	def test_criando_cores
+		@rand_cores.rgb(10)
+	end
+	
+end
